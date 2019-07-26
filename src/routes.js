a0eaf53a112694db24994982d5c887841f3c2f9f
Hiago Souza <hiagofss98@gmail.com>
2019-07-25 21:31:55 -0300

    Criando rota GET

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,10 @@ const routes = new Router();
 
 const MusicController = require('./app/controllers/MusicController');
 
+routes.get('/', (req, res) => {
+  res.send('Digite uma cidade na URL');
+});
+
 routes.get('/:city', MusicController.index);
 
 module.exports = routes;
