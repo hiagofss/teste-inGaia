f5b783e9372bad9bdc3ae1d0bfa68fb532013394
Hiago Souza <hiagofss98@gmail.com>
2019-07-25 21:49:44 -0300

    Add to README.md

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = new Router();
 const MusicController = require('./app/controllers/MusicController');
 
 routes.get('/', (req, res) => {
-  res.send('Digite uma cidade na URL');
+  res.send('<h1>Digite uma cidade na URL</h1>');
 });
 
 routes.get('/:city', MusicController.index);
